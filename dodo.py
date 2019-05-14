e75c647a3f62b266a9db272e29a70d58b44a85fc
Julia Signell <jsignell@gmail.com>
2019-05-14 16:35:57 -0400

    Leave tests in example config

--- dodo.py
+++ dodo.py
@@ -13,23 +13,3 @@ def task_ecosystem_setup():
         "conda update conda",
         "conda install anaconda-project 'tornado<5.0'",
     ]}
-
-
-def task_lint():
-    """Generic lint command starting from within example directory"""
-    return {'actions': [
-        'anaconda-project prepare',
-        'conda activate ./envs/default',
-        'conda install -c pyviz nbsmoke pytest',
-        'pytest --nbsmoke-lint -k *.ipynb --ignore envs',
-    ]}
-
-
-def task_test():
-    """Generic lint command starting from within example directory"""
-    return {'actions': [
-        'anaconda-project prepare',
-        'conda activate ./envs/default',
-        'conda install -c pyviz nbsmoke pytest',
-        'pytest --nbsmoke-run -k *.ipynb --ignore envs',
-    ]}
