0c25b9d8a507971377f6b84405f66fe4e05150a3
Julia Signell <jsignell@gmail.com>
2019-05-20 12:53:35 -0400

    Copy the test data into the example dir before running tests

--- dodo.py
+++ dodo.py
@@ -1,4 +1,6 @@
 import os
+from distutils.dir_util import copy_tree
+import filecmp
 
 if "PYCTDEV_ECOSYSTEM" not in os.environ:
     os.environ["PYCTDEV_ECOSYSTEM"] = "conda"
@@ -13,3 +15,73 @@ def task_ecosystem_setup():
         "conda update conda",
         "conda install anaconda-project 'tornado<5.0'",
     ]}
+
+
+name_param = {
+    'name': 'name',
+    'long': 'name',
+    'type': str,
+    'default': 'attractors'
+}
+
+def _prepare_paths(root, name, test_data):
+    if root == '':
+        root = os.getcwd()
+    root = os.path.abspath(root)
+    test_data = test_data if os.path.isabs(test_data) else os.path.join(root, test_data)
+    return {
+        'real': os.path.join(root, name, 'data'),
+        'test': os.path.join(test_data, name),
+    }
+
+
+def task_small_data_setup():
+    """Copy small versions of the data from test_data"""
+
+    def copy_test_data(root='', name='attractors', test_data='test_data'):
+        print('Copying test data for {} ...'.format(name))
+        paths = _prepare_paths(root, name, test_data)
+
+        if not os.path.exists(paths['test']):
+            print("Fail: No test_data found for {} in {}".format(name, paths['test']))
+            return
+
+        if os.path.exists(paths['real']) and os.listdir(paths['real']):
+            matching_files = filecmp.dircmp(paths['test'], paths['real']).same_files
+            if os.listdir(paths['real']) != matching_files:
+                print("Fail: Data files already exist in {}".format(paths['real']))
+                return
+            else:
+                print("Nothing to do: Test data already in {}".format(paths['real']))
+                return
+        copy_tree(paths['test'], paths['real'])
+        print("Done!")
+
+    return {'actions': [copy_test_data], 'params': [name_param]}
+
+
+def task_small_data_cleanup():
+    """Experimental: Replace the test version of the catalog with the real one"""
+
+    def remove_test_data(root='', name='attractors', test_data='test_data'):
+        print('Removing test data for {} ...'.format(name))
+        paths = _prepare_paths(root, name, test_data)
+
+        if not os.path.exists(paths['test']):
+            print("Nothing to do: No test_data found for {} in {}".format(name, paths['test']))
+            return
+
+        if not os.path.exists(paths['real']) or not os.listdir(paths['real']):
+            print("Nothing to do: No data found in {}".format(paths['real']))
+            return
+
+        matching_files = filecmp.dircmp(paths['test'], paths['real']).same_files
+        if os.listdir(paths['real']) != matching_files:
+            print("Fail: Data files at {} are not identical to test, so they shouldn't be deleted.".format(paths['real']))
+            return
+
+        for filename in matching_files:
+            os.remove(os.path.join(paths['real'], filename))
+        print("Done!")
+
+    return {'actions': [remove_test_data], 'params': [name_param]}
