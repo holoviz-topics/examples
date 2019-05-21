9fbfa471fd4126300c0ec4da9552bc272ef358b3
Julia Signell <jsignell@gmail.com>
2019-05-21 16:51:25 -0400

    Moving osm and adding test_data (#15)
    * Adding anaconda-project file and moving notebook
    
    * Setting up infrastructure for running tests
    
    * Adding test data [only:osm]
    
    * Doing better logic in dodo [only:osm]

--- dodo.py
+++ dodo.py
@@ -1,6 +1,7 @@
 import os
 from distutils.dir_util import copy_tree
 import filecmp
+import shutil
 
 if "PYCTDEV_ECOSYSTEM" not in os.environ:
     os.environ["PYCTDEV_ECOSYSTEM"] = "conda"
@@ -33,6 +34,35 @@ def _prepare_paths(root, name, test_data):
         'real': os.path.join(root, name, 'data'),
         'test': os.path.join(test_data, name),
     }
+
+# From https://stackoverflow.com/a/24860799/4021797
+class dircmp(filecmp.dircmp):
+    """
+    Compare the content of dir1 and dir2. In contrast with filecmp.dircmp, this
+    subclass compares the content of files with the same path.
+    """
+    def phase3(self):
+        """
+        Find out differences between common files.
+        Ensure we are using content comparison with shallow=False.
+        """
+        fcomp = filecmp.cmpfiles(self.left, self.right, self.common_files,
+                                 shallow=False)
+        self.same_files, self.diff_files, self.funny_files = fcomp
+
+def is_same(dir1, dir2):
+    """
+    Compare two directory trees content.
+    Return False if they differ, True is they are the same.
+    """
+    compared = dircmp(dir1, dir2)
+    if (compared.left_only or compared.right_only or compared.diff_files
+        or compared.funny_files):
+        return False
+    for subdir in compared.common_dirs:
+        if not is_same(os.path.join(dir1, subdir), os.path.join(dir2, subdir)):
+            return False
+    return True
 
 
 def task_small_data_setup():
@@ -79,14 +109,10 @@ def task_small_data_cleanup():
             os.rmdir(paths['real'])
             return
 
-        matching_files = filecmp.dircmp(paths['test'], paths['real']).same_files
-        if set(os.listdir(paths['real'])) != set(matching_files):
+        if not is_same(paths['test'], paths['real']):
             raise ValueError("Fail: Data files at {} are not identical to test, so they shouldn't be deleted.".format(paths['real']))
 
-        for filename in matching_files:
-            os.remove(os.path.join(paths['real'], filename))
-
-        os.rmdir(paths['real'])
+        shutil.rmtree(paths['real'])
         print("Done!")
 
     return {'actions': [remove_test_data], 'params': [name_param]}
