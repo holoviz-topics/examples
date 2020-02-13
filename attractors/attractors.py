965807ea0e52eed084641de24f85f8afcb89ba82
James A. Bednar <jbednar@continuum.io>
2020-02-13 13:56:53 -0600

    Fixed lint and the remember option

--- attractors/attractors.py
+++ attractors/attractors.py
@@ -14,7 +14,6 @@ from collections import OrderedDict
 import numpy as np
 import pandas as pd
 import param
-import panel as pn
 import inspect
 import yaml
 
@@ -51,7 +50,7 @@ def trajectory(fn, x0, y0, a, b=None, c=None, d=None, e=None, f=None, n=1000000)
 
 class Attractor(param.Parameterized):
     """Base class for a Parameterized object that can evaluate an attractor trajectory"""
-    
+
     x = param.Number(0,  softbounds=(-2, 2), doc="Starting x value", precedence=-1)
     y = param.Number(0,  softbounds=(-2, 2), doc="Starting y value", precedence=-1)
 
@@ -64,16 +63,16 @@ class Attractor(param.Parameterized):
 
     equations = param.List([], class_=str, precedence=-1, readonly=True, doc="""
         LaTeX-formatted list of equations""")
-    
+
     __abstract = True
-    
+
     def __call__(self, n, x=None, y=None):
         """Return a dataframe with *n* points"""
         if x is not None: self.x=x
         if y is not None: self.y=y
         args = [getattr(self,p) for p in self.sig()]
         return trajectory(self.fn, *args, n=n)
-    
+
     def vals(self):
         return [self.__class__.name] + [self.colormap] + [getattr(self,p) for p in self.sig()]
 
@@ -91,9 +90,9 @@ class FourParamAttractor(Attractor):
 
 
 class Clifford(FourParamAttractor):
-    equations = param.List([r'$x_{n+1} = \sin\ ay_n + c\ \cos\ ax_n$', 
+    equations = param.List([r'$x_{n+1} = \sin\ ay_n + c\ \cos\ ax_n$',
                             r'$y_{n+1} = \sin\ bx_n + d\ \cos\ by_n$'])
-    
+
     @staticmethod
     @jit(nopython=True)
     def fn(x, y, a, b, c, d, *o):
@@ -102,54 +101,54 @@ class Clifford(FourParamAttractor):
 
 
 class De_Jong(FourParamAttractor):
-    equations = param.List([r'$x_{n+1} = \sin\ ay_n - c\ \cos\ bx_n$', 
+    equations = param.List([r'$x_{n+1} = \sin\ ay_n - c\ \cos\ bx_n$',
                             r'$y_{n+1} = \sin\ cx_n - d\ \cos\ dy_n$'])
-    
+
     @staticmethod
     @jit(nopython=True)
     def fn(x, y, a, b, c, d, *o):
         return sin(a * y) - cos(b * x), \
                sin(c * x) - cos(d * y)
 
-    
+
 class Svensson(FourParamAttractor):
-    equations = param.List([r'$x_{n+1} = d\ \sin\ ax_n - \sin\ by_n$', 
+    equations = param.List([r'$x_{n+1} = d\ \sin\ ax_n - \sin\ by_n$',
                             r'$y_{n+1} = c\ \cos\ ax_n + \cos\ by_n$'])
-    
+
     @staticmethod
     @jit(nopython=True)
     def fn(x, y, a, b, c, d, *o):
         return d * sin(a * x) - sin(b * y), \
                c * cos(a * x) + cos(b * y)
 
-    
+
 class Fractal_Dream(Attractor):
-    equations = param.List([r'$x_{n+1} = \sin\ by_n + c\ \sin\ bx_n$', 
+    equations = param.List([r'$x_{n+1} = \sin\ by_n + c\ \sin\ bx_n$',
                             r'$y_{n+1} = \sin\ ax_n + d\ \sin\ ay_n$'])
-    
+
     c = param.Number(1.15, softbounds=(-0.5, 1.5), doc="Attractor parameter c")
     d = param.Number(2.34, softbounds=(-0.5, 1.5), doc="Attractor parameter d")
-    
+
     @staticmethod
     @jit(nopython=True)
     def fn(x, y, a, b, c, d, *o):
         return sin(b*y)+c*sin(b*x), \
                sin(a*x)+d*sin(a*y)
 
-    
+
 class Bedhead(Attractor):
-    equations = param.List([r'$x_{n+1} = y_n\ \sin\ \frac{x_ny_n}{b} + \cos(ax_n-y_n)$', 
+    equations = param.List([r'$x_{n+1} = y_n\ \sin\ \frac{x_ny_n}{b} + \cos(ax_n-y_n)$',
                             r'$y_{n+1} = x_n+\frac{\sin\ y_n}{b}$'])
-    
+
     a = param.Number(0.64, bounds=(-1, 1))
     b = param.Number(0.76, bounds=(-1, 1))
-    
+
     @staticmethod
     @jit(nopython=True)
     def fn(x, y, a, b, *o):
         return y*sin(x*y/b) + cos(a*x-y), \
                x + sin(y)/b
-    
+
     def __call__(self, n):
         # Avoid interactive divide-by-zero errors for b
         epsilon = 3*np.finfo(float).eps
@@ -157,11 +156,11 @@ class Bedhead(Attractor):
             self.b = epsilon
         return super(Bedhead,self).__call__(n)
 
-    
+
 class Hopalong1(Attractor):
-    equations = param.List([r'$x_{n+1} = y_n-\mathrm{sgn}(x_n)\sqrt{\left|\ bx_n-c\ \right|}$', 
+    equations = param.List([r'$x_{n+1} = y_n-\mathrm{sgn}(x_n)\sqrt{\left|\ bx_n-c\ \right|}$',
                             r'$y_{n+1} = a-x_n$'])
-    
+
     a = param.Number(9.8, bounds=(0, 10))
     b = param.Number(4.1, bounds=(0, 10))
     c = param.Number(3.8, bounds=(0, 10), doc="Attractor parameter c")
@@ -174,9 +173,9 @@ class Hopalong1(Attractor):
 
 
 class Hopalong2(Hopalong1):
-    equations = param.List([r'$x_{n+1} = y_n-1-\mathrm{sgn}(x_n-1)\sqrt{\left|\ bx_n-1-c\ \right|}$', 
+    equations = param.List([r'$x_{n+1} = y_n-1-\mathrm{sgn}(x_n-1)\sqrt{\left|\ bx_n-1-c\ \right|}$',
                             r'$y_{n+1} = a-x_n-1$'])
-    
+
     @staticmethod
     @jit(nopython=True)
     def fn(x, y, a, b, c, *o):
@@ -190,15 +189,15 @@ def G(x, mu):
 
 class Gumowski_Mira(Attractor):
     equations = param.List([r'$G(x) = \mu x + \frac{2(1-\mu)x^2}{1+x^2}$',
-                            r'$x_{n+1} = y_n + ay_n(1-by_n^2) + G(x_n)$', 
+                            r'$x_{n+1} = y_n + ay_n(1-by_n^2) + G(x_n)$',
                             r'$y_{n+1} = -x_n + G(x_{n+1})$'])
-    
+
     x = param.Number(0,    softbounds=(-20, 20), doc="Starting x value", precedence=0.1)
     y = param.Number(0,    softbounds=(-20, 20), doc="Starting y value", precedence=0.1)
     a = param.Number(0.64, softbounds=( -1,  1))
     b = param.Number(0.76, softbounds=( -1,  1))
     mu = param.Number(0.6, softbounds=( -2,  2), doc="Attractor parameter mu")
-    
+
     @staticmethod
     @jit(nopython=True)
     def fn(x, y, a, b, mu, *o):
@@ -221,14 +220,14 @@ class Symmetric_Icon(Attractor):
         zzbar = x*x + y*y
         p = a*zzbar + l
         zreal, zimag = x, y
-    
+
         for i in range(1, d-1):
             za, zb = zreal * x - zimag * y, zimag * x + zreal * y
             zreal, zimag = za, zb
-    
+
         zn = x*zreal - y*zimag
         p += b*zn
-    
+
         return p*x + g*zreal - om*y, \
                p*y - g*zimag + om*x
 
@@ -236,13 +235,14 @@ class Symmetric_Icon(Attractor):
 class ParameterSets(param.Parameterized):
     """
     Allows selection from sets of pre-defined parameters saved in YAML.
-    
+
     Assumes the YAML file returns a list of groups of values.
     """
 
     examples_filename = param.Filename("attractors.yml")
+    current           = param.Callable(lambda: None, precedence=-1)
     remember_this_one = param.Action(lambda x: x._remember())
-    
+
     load      = param.Action(lambda x: x._load())
     randomize = param.Action(lambda x: x._randomize())
     sort      = param.Action(lambda x: x._sort())
@@ -252,30 +252,31 @@ class ParameterSets(param.Parameterized):
     def __init__(self,**params):
         super(ParameterSets,self).__init__(**params)
         self._load()
-        
+
         self.attractors = OrderedDict(sorted([(k,v(name=k + " parameters")) for k,v in concrete_descendents(Attractor).items()]))
         for k in self.attractors:
             self.attractor(k, *self.args(k)[0])
 
     def _load(self):
-        with open(self.examples_filename,"r") as f: 
+        with open(self.examples_filename,"r") as f:
             vals = yaml.safe_load(f)
             assert(vals and len(vals)>0)
             self.param.example.objects=vals
             self.example = vals[0]
 
     def _save(self):
-        with open(self.examples_filename,"w") as f: 
+        with open(self.examples_filename,"w") as f:
             yaml.dump(self.param.example.objects,f)
 
     def __call__(self):        return self.example
     def _randomize(self):      npr.shuffle(self.param.example.objects)
     def _sort(self):            self.param.example.objects = list(sorted(self.param.example.objects))
     def _add_item(self, item): self.param.example.objects += [item] ; self.example=item
+
     def _remember(self):
-        vals = ats.attractor_type.vals() # forward reference
+        vals = self.current().vals()
         self._add_item(vals)
-        
+
     def args(self, name):
         return [v[1:] for v in self.param.example.objects if v[0]==name]
 
