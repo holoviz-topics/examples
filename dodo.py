a2b710959773217e3fd0a79a1fe241f664c3cf06
jlstevens <jlrstevens@gmail.com>
2020-02-27 14:54:58 -0600

    Listing builtdocs

--- dodo.py
+++ dodo.py
@@ -286,7 +286,7 @@ def task_index_symlinks():
             project_path = os.path.join('doc', name)
             print(name)
             print(project_path)
-            print(os.listdir(name))
+            print(os.listdir('builtdocs', name))
 
 
     return {'actions':[generate_index_symlinks]}
