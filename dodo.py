95db9fbbe3740ec1b6c497499748831fa447ae41
jlstevens <jlrstevens@gmail.com>
2021-01-29 11:43:44 +0000

    Reverted to using os.symlink

--- dodo.py
+++ dodo.py
@@ -289,9 +289,8 @@ def task_index_symlinks():
                 os.chdir(project_path)
                 listing = os.listdir(project_path)
                 if 'index.html' not in listing:
-                    os.system('ln -s %s.html index.html' % name)
+                    os.symlink('./%s.html' % name, './index.html')
                     print('Created symlink for %s' % name)
-                    print(os.listdir('.'))
                 os.chdir(cwd)
             except Exception as e:
                 print(str(e))
